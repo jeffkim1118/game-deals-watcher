0f1492122c7385f43b5ac05c87b49b272a762aba
yoonsung <jeffkim1118@outlook.com>
2022-07-13 14:57:59 -0400

    Setting up gemfile

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,17 @@ module GameDealsWatcher
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    config.middleware.use ActionDispatch::cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    config.action_dispatch.cookies_same_site_protection = :strict
+    
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
@@ -31,9 +42,6 @@ module GameDealsWatcher
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    
   end
 end
